1d1b5a000b4919b2fc1102c4099b3c551b0896e0
Franco-Buonfrate <81194289+Franco-Buonfrate@users.noreply.github.com>
2021-10-26 22:25:44 -0300

    Delete Buonfrate.Franco.p1LabI1A directory

--- Buonfrate.Franco.p1LabI1A/dataWereHouse.h
+++
@@ -1,9 +0,0 @@
-#include "bicicleta.h"
-#ifndef DATAWAREHOUSE_H_INCLUDED
-#define DATAWAREHOUSE_H_INCLUDED
-
-
-int hardcodearBicicletas(eBici lista[], int tam, int cant, int* pId);
-
-#endif // DATAWAREHOUSE_H_INCLUDED
-
